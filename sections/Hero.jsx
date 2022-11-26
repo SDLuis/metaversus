af71e89a2a785bd48c20c43a48093e7834eba4ec
Luis <Luistejeda0011@gmail.com>
2022-11-26 14:46:04 -0400

    Working Hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,47 @@
 'use client'
 
+import { motion } from 'framer-motion'
+
+import styles from '../styles'
+import { slideIn, staggerContainer, textVariant } from '../utils/motion'
+
 const Hero = () => (
-  <section>
-    Hero section
+  <section className='sm:py-10 xs:py-8 py-12 sm:pl-10 pl-6'>
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className='flex justify-center items-center flex-col relative z-10'>
+        <motion.h1
+          variants={textVariant(1.2)}
+          className={`${styles.heroHeading}`}
+        >
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.1)}
+          className='flex flex-row lg:-mt-5 justify-center items-center'
+        >
+          <div className={`${styles.heroHeading}`}>Ma</div>
+          <div className={`${styles.heroDText}`} />
+          <div className={`${styles.heroHeading}`}>Ness</div>
+        </motion.div>
+      </div>
+      <div className='w-full flex justify-center'>
+        <motion.div
+          variants={slideIn('right', 'tween', 0.2, 1)}
+          className='relative w-full md:w-[80%] md:-mt-[13px] lg:-mt-[16px]'
+          initial='hidden'
+          whileInView='show'
+        >
+          <div className='absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]' />
+          <img src='/cover.png' alt='cover' className='w-full -mt-2 sm:-mt-0 sm:h-[350px] h-[350px] object-cover rounded-tl-[140px] z-10 relative' />
+        </motion.div>
+      </div>
+    </motion.div>
   </section>
 )
 
--- styles/index.js
+++ styles/index.js
@@ -4,7 +4,7 @@ const styles = {
 
   paddings: 'sm:p-16 xs:p-8 px-6 py-12',
   yPaddings: 'sm:py-16 xs:py-8 py-12',
-  xPaddings: 'sm:px-16 px-6',
+  xPaddings: 'sm:px-32 px-6',
   topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
   bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
 
@@ -15,9 +15,9 @@ const styles = {
 
   // hero section
   heroHeading:
-      'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
+      'font-bold md:text-[100px] sm:text-[60px] text-[44px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
   heroDText:
-      'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+      'md:w-[212px] sm:w-[80px] w-[60px] md:h-[80px] sm:h-[48px] h-[34px] md:border-[16px] sm:border-[9px] border-[7px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
 }
 
 export default styles
