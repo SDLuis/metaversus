08bffd1fb503a0923f99318fead4624abb70fdc5
Luis <Luistejeda0011@gmail.com>
2022-11-26 15:38:50 -0400

    Adding stamp img to hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { motion } from 'framer-motion'
-
 import styles from '../styles'
 import { slideIn, staggerContainer, textVariant } from '../utils/motion'
 
@@ -39,6 +38,15 @@ const Hero = () => (
         >
           <div className='absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]' />
           <img src='/cover.png' alt='cover' className='w-full -mt-2 sm:-mt-0 sm:h-[350px] h-[350px] object-cover rounded-tl-[140px] z-10 relative' />
+          <a href='#explore'>
+            <div className='w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10'>
+              <img
+                src='/stamp.png'
+                alt='stamp'
+                className='sm:w-[135px] w-[100px] sm:h-[135px] h-[100px] object-contain'
+              />
+            </div>
+          </a>
         </motion.div>
       </div>
     </motion.div>
